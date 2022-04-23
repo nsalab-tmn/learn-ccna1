b11dafb626d83dbdcda73d7049f7bdc6f8b6d545
Aleksandr Gorbachev <menus12@mail.ru>
2022-04-23 13:46:50 +0500

    fix script

--- words.sh
+++ words.sh
@@ -2,8 +2,13 @@
 
 avg=120
 
-for file in ./*/lecture.md; do
-    number_of_words=`wc --word < $file`
+for path in ./*/; do
+    lmd_path=${path}lecture.md
+    number_of_words=`wc --word < $lmd_path`
     number_of_minutes=$((number_of_words / avg))
-    echo "$file": "$number_of_words" words - "$number_of_minutes" minutes of reading
+    nom_lz=$(printf "%02d" $number_of_minutes)
+    echo "$lmd_path": "$number_of_words" words - "$nom_lz" minutes of reading
+
+    jmt_path=${path}learn-metadata.json
+    cat "$jmt_path" | jq -r .duration
 done
