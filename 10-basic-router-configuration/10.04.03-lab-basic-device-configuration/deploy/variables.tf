110379c96358e4969330e61d18440a6fba744ea2
KEFedotov <KFedotov@nsalab.org>
2022-08-04 10:07:07 +0300

    Update track 10 labs

---
+++ 10-basic-router-configuration/10.04.03-lab-basic-device-configuration/deploy/variables.tf
@@ -0,0 +1,19 @@
+variable "instance_id" {
+    type        = string
+    default     = "user01_1111"
+}
+
+variable "tp_name" {
+    type        = string
+    default     = "smpl_lab"
+}
+
+variable "tp_learn_env" {
+    type        = string
+    default     = "dev"
+}
+
+variable "tp_learn_user" {
+    type        = string
+    default     = "user01"
+}
