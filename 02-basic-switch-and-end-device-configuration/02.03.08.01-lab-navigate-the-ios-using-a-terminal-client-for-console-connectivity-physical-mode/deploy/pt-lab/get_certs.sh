898d9c8adef514026d4b3aeacc17a9ecc21f99b0
KEFedotov <KFedotov@nsalab.org>
2022-10-15 00:05:02 +0300

    Update get_certs.sh

--- 02-basic-switch-and-end-device-configuration/02.03.08.01-lab-navigate-the-ios-using-a-terminal-client-for-console-connectivity-physical-mode/deploy/pt-lab/get_certs.sh
+++ 02-basic-switch-and-end-device-configuration/02.03.08.01-lab-navigate-the-ios-using-a-terminal-client-for-console-connectivity-physical-mode/deploy/pt-lab/get_certs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o $LOCAL_PATH/supercert.pem https://learncertstore.blob.core.windows.net/supercert/supercert.pem
-curl -o $LOCAL_PATH/supercert.key https://learncertstore.blob.core.windows.net/supercert/supercert.key
+curl -o $LOCAL_PATH/supercert.pem https://learncertstore.blob.core.windows.net/supercert/supercert.pem --retry 3 --retry-all-errors --retry-delay 2
+curl -o $LOCAL_PATH/supercert.key https://learncertstore.blob.core.windows.net/supercert/supercert.key --retry 3 --retry-all-errors --retry-delay 2
