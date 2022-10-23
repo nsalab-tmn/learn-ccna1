a73e8dc8eb002b191c280e0167adc28653a449da
KEFedotov <KFedotov@nsalab.org>
2022-10-23 16:29:54 +0300

    Deployer + assessor module 3

---
+++ 03-protocols-and-models/03.05.05-lab-investigate-the-tcp-ip-and-osi-models-in-action/deploy/pt-lab/provision_files.tf
@@ -0,0 +1,45 @@
+resource "null_resource" "lab" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = var.root_password
+      host     = vkcs_compute_instance.compute[0].access_ip_v4
+
+    }
+    provisioner "remote-exec" {
+      inline = [
+        "#!/bin/bash",
+        "mkdir -p /opt/nsalab/lab"
+      ]
+    }
+    provisioner "file" {
+      source      = "${path.module}/3.5.5-lab.pka"
+      destination = "/opt/nsalab/lab/lab.pka"
+    }
+
+    depends_on = [
+      vkcs_compute_instance.compute
+    ]
+}
+
+resource "null_resource" "certs" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = var.root_password
+      host     = vkcs_compute_instance.compute[0].access_ip_v4
+
+    }
+    provisioner "file" {
+      source      = "${path.module}/supercert.pem"
+      destination = "/home/sysadmin/.vnc/supercert.pem"
+    }
+    provisioner "file" {
+      source      = "${path.module}/supercert.key"
+      destination = "/home/sysadmin/.vnc/supercert.key"
+    }
+    depends_on = [
+      null_resource.lab
+    ]
+}
+
